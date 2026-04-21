e5d595bb1150691a05ea9137a735f94874d9ce1a
Gustavo Miller Santos <g248320@dac.unicamp.br>
2026-04-21 19:37:15 -0300

    feat: temporarily add world directly to main class
    This should be reverted when we have the router

--- space/lib/main.dart
+++ space/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:space/src/game/entities/player.dart';
+import 'package:space/src/game/control_panel/control_panel_world.dart';
 
 void main() {
   runApp(
@@ -12,9 +13,32 @@ void main() {
 }
 
 class MyGame extends FlameGame {
+  World? _activeWorld;
+
+  @override
+  Color backgroundColor() => const Color(0xFF060A1A);
+
   @override
   Future<void> onLoad() async {
-    // Add player at center of screen
-    add(Player());
+    await super.onLoad();
+    camera.viewfinder.anchor = Anchor.topLeft;
+    camera.viewfinder.position = Vector2.zero();
+    await openRoute('control_panel');
+  }
+
+  Future<void> openRoute(String routeId) async {
+    final World nextWorld = switch (routeId) {
+      'control_panel' => ControlPanelWorld(),
+      _ => ControlPanelWorld(),
+    };
+
+    final World? previousWorld = _activeWorld;
+    if (previousWorld != null) {
+      previousWorld.removeFromParent();
+    }
+
+    _activeWorld = nextWorld;
+    camera.world = nextWorld;
+    await add(nextWorld);
   }
 }
